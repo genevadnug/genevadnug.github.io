f055e4999a4a6bdaaeb8fb10b4321805b0dece54
Tor <tor.knutsson@gmail.com>
2015-09-30 09:07:30 +0200

    Update gvadnug.js
    Adds try-catch-proxy for meetup api calls

--- js/gvadnug.js
+++ js/gvadnug.js
@@ -73,51 +73,60 @@ $(function () {
             });
         }
     };
-    $.getJSON('https://api.meetup.com/Geneva-NET-User-Group?photo-host=public&page=20&sig_id=67833332&sig=ef55990d355bf44d22624e35aa3b62424dd59ea4&callback=?',
-        callbackFactory("data-meetup-group")
-        );
+	
+	$.getJSON('/data/meetup-api-urls.json'), 
+		function(urls) { 
+			var getJSONOrProxy = function(urlName, callback) {
+				try {
+					return $.getJSON(urls[urlName] + "&callback=?", callback);
+				}
+				catch{
+					return $.getJSON("http://meetup.nattkul.com/?url-key=" + urlName + "&callback=?", callback);
+				}
+			};
+			
+			getJSONOrProxy('group', callbackFactory("data-meetup-group"));
 
-     $.getJSON('https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Geneva-NET-User-Group&photo-host=public&page=20&fields=&order=time&desc=false&status=upcoming&sig_id=67833332&sig=c159d829d9aafad843c08bf625b1593d88546c01&callback=?',
-        callbackFactory("data-meetup-next-event"));
+			getJSONOrProxy('nextEvent', callbackFactory("data-meetup-next-event"));
 
-    var squareUrlProvider = 'https://images.weserv.nl/?h=60&w=60&t=square&url=';
-    $.getJSON('https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Geneva-NET-User-Group&photo-host=public&page=4&fields=&order=time&status=past&desc=true&sig_id=67833332&sig=bed632114c88e057c669cb33777ac2789fb8aa18&callback=?',
-      callbackFactory("data-meetup-past-events"))
-    .done(function () {
-        var eventIds = [];
-        var eventIdStrArr = [];
-        $("[data-meetup-event-id]").each(function () {
-            var eventId = $(this).data("meetupEventId");
-            eventIds[eventId] = 1;
-            eventIdStrArr.push(eventId);
-        });
+			var squareUrlProvider = 'https://images.weserv.nl/?h=60&w=60&t=square&url=';
+			getJSONOrProxy('pastEvents', callbackFactory("data-meetup-past-events"))
+			.done(function () {
+				var eventIds = [];
+				var eventIdStrArr = [];
+				$("[data-meetup-event-id]").each(function () {
+					var eventId = $(this).data("meetupEventId");
+					eventIds[eventId] = 1;
+					eventIdStrArr.push(eventId);
+				});
 
-        var eventUrlString = eventIdStrArr.join("%2C");
-        
-        if (eventIds.length > 0) {
-            $.getJSON('https://api.meetup.com/2/photos?offset=0&format=json&group_urlname=Geneva-NET-User-Group&only=thumb_link%2Cphoto_album&photo-host=public&page=20&fields=&order=time&desc=True&sig_id=67833332&sig=d8f4c3dd484801ed30ce5ba1fe7261723e109e4a&callback=?',
-                function (imgData) {
-                    if (imgData.results) {
-                        for (var i = 0; i < imgData.results.length; i++) {
-                            var currImgData = imgData.results[i];
-                            var eventId = currImgData.photo_album.event_id;
+				var eventUrlString = eventIdStrArr.join("%2C");
+				
+				if (eventIds.length > 0) {
+					$.getJSON('thumbnails',
+						function (imgData) {
+							if (imgData.results) {
+								for (var i = 0; i < imgData.results.length; i++) {
+									var currImgData = imgData.results[i];
+									var eventId = currImgData.photo_album.event_id;
 
-                            // Only take first image for each event.
-                            if (eventId && eventIds[eventId])
-                                delete eventIds[eventId];
-                            else {
-                                continue;
-                            }
+									// Only take first image for each event.
+									if (eventId && eventIds[eventId])
+										delete eventIds[eventId];
+									else {
+										continue;
+									}
 
-                            var $img = $("[data-meetup-event-id='" + eventId + "'] img");
-                            $img.attr("src", squareUrlProvider + currImgData.thumb_link.replace(/.*?:\/\//g, "")); $img
-                            $img.attr("title", "A pic from this event");
-                            $img.attr("alt", "A pic from this event.");
-                        }
-                    }
-                });
-        }
-    });
+									var $img = $("[data-meetup-event-id='" + eventId + "'] img");
+									$img.attr("src", squareUrlProvider + currImgData.thumb_link.replace(/.*?:\/\//g, "")); $img
+									$img.attr("title", "A pic from this event");
+									$img.attr("alt", "A pic from this event.");
+								}
+							}
+						});
+				}
+			});
+	});
 
 });
 
